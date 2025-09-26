213b1694f4876aa4db6ccdeea7513e4fa6043fe5
lhoju0158 <lhoju0158@gmail.com>
2025-09-26 16:09:02 +0900

    [ICC-146] api_server.js 수정

--- single_server/api_server.js
+++ single_server/api_server.js
@@ -5,34 +5,64 @@ import Hashids from "https://cdn.jsdelivr.net/npm/hashids@2.3.0/+esm";
 
 const BASE_URL = 'https://api.q-asker.com'
 const PROBLEM_SET_ID_MAX = 1156
+const CRITERION = 200 // ms
 
 const problemSetGenerationRequestDuration = new Trend("problem_set_generation_duration");
 const problemSetGetRequestDuration = new Trend("problem_set_get_duration");
 const explanationGetRequestDuration = new Trend("explanation_get_duration");
 
-const checkSuccess = new Counter("check_success");
-const checkFail = new Counter("check_fail");
+const generationSuccess = new Counter("gen_success");
+const generationFail = new Counter("gen_fail");
+const generationUnder = new Counter("gen_under")
+const generationOver = new Counter("gen_over")
+
+const problemSetGetSuccess = new Counter("prob_success");
+const problemSetGetFail = new Counter("prob_fail");
+const problemSetGetUnder = new Counter("prob_under")
+const problemSetGetOver = new Counter("prob_over")
+
+const explanationSuccess = new Counter("exp_success");
+const explanationFail = new Counter("exp_fail");
+const explanationUnder = new Counter("exp_under")
+const explanationOver = new Counter("exp_over")
 
 export const options = {
   scenarios: {
-    load_pattern: {
-      executor: "ramping-arrival-rate",
-      startRate: 10,        // 시작은 분당 10 req (≈0.17 RPS)
-      timeUnit: "1m",       // rate 기준 단위
-      preAllocatedVUs: 20,  // 충분히 크게 잡기
-      maxVUs: 100,
-
-      stages: [
-        { target: 79, duration: "5m" },  // Ramp-up: 10 → 79 req/min (≈1.31 RPS)
-        { target: 79, duration: "5m" },  // Peak 유지
-        { target: 10, duration: "5m" },  // Ramp-down
-      ],
-    },
+  // warm up - 5분 동안 약 0.5 RPS 유지 (서서히 준비 단계)
+  warm_up: {
+    executor: "constant-arrival-rate",
+    rate: 0.5,              // 초당 0.5 요청
+    duration: "5m",
+    timeUnit: "1s",
+    preAllocatedVUs: 2,
+    maxVUs: 10,
   },
+  // peak load - 5분 동안 1.5 RPS 유지
+  high_peak_load: {
+    executor: "constant-arrival-rate",
+    rate: 1.5,             // 초당 1.5 요청
+    duration: "5m",
+    timeUnit: "1s",
+    startTime: "5m",
+    preAllocatedVUs: 5,
+    maxVUs: 20,
+  },
+  // cool down - 다시 0.5 RPS로 감소
+  cool_down: {
+    executor: "constant-arrival-rate",
+    rate: 0.5,              // 초당 0.5 요청
+    duration: "5m",
+    timeUnit: "1s",
+    startTime: "10m",
+    preAllocatedVUs: 2,
+    maxVUs: 10,
+  },
+},
+
   thresholds: {
-    problem_set_generation_duration: ["avg<200"], 
-    problem_set_get_duration: ["avg<200"], 
-    explanation_get_duration: ["avg<200"], 
+    problem_set_generation_duration: [`avg<${CRITERION}`], 
+    problem_set_get_duration: [`avg<${CRITERION}`], 
+    explanation_get_duration: [`avg<${CRITERION}`], 
   },
 }
 function generateRandomProblemSetId(){
@@ -58,30 +88,22 @@ export default function () {
         headers: { 'Content-Type': 'application/json' }
     });
     problemSetGenerationRequestDuration.add(generationRes.timings.duration);
-    
-    if (generationRes.status === 200) {
-        checkSuccess.add(1, { check: "문제 생성 성공" });
-    } else {
-        checkFail.add(1, { check: "문제 생성 성공" });
-    }    
+    generationRes.status === 200  ? generationSuccess.add(1) : generationFail.add(1);
+    (generationRes.timings.duration<CRITERION&&generationRes.status === 200) ? generationUnder.add(1) : generationOver.add(1);
+
     const problemSetId = generateRandomProblemSetId();
 
     // 2. 생성된 problemSetId를 이용한 문제 세트 가져오기 테스트
     const problemSetUrl = `${BASE_URL}/problem-set/:${problemSetId}`;
     const problemSetRes = http.get(problemSetUrl);
     problemSetGetRequestDuration.add(problemSetRes.timings.duration);
-    if (problemSetRes.status === 200) {
-        checkSuccess.add(1, { check: "문제 세트 가져오기 성공" });
-    } else {
-        checkFail.add(1, { check: "문제 세트 가져오기 성공" });
-    }
+    problemSetRes.status === 200  ? problemSetGetSuccess.add(1) : problemSetGetFail.add(1);
+    (problemSetRes.timings.duration<CRITERION&&problemSetRes.status === 200) ? problemSetGetUnder.add(1) : problemSetGetOver.add(1);
 
     // 3. 생성된 problemSetId를 이용한 해설 가져오기 테스트
     const explanationUrl = `${BASE_URL}/explanation/:${problemSetId}`;
     const explanationRes = http.get(explanationUrl);
     explanationGetRequestDuration.add(explanationRes.timings.duration);
-    if (problemSetRes.status === 200) {
-        checkSuccess.add(1, { check: "문제 세트 가져오기 성공" });
-    } else {
-        checkFail.add(1, { check: "문제 세트 가져오기 성공" });
-    }}
+    generationRes.status === 200  ? explanationSuccess.add(1) : explanationFail.add(1);
+    (generationRes.timings.duration<CRITERION&&generationRes.status === 200) ? explanationUnder.add(1) : explanationOver.add(1);
+  }
